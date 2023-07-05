d34ec84744dd92190cbb202e9d45511617d82164
gaddyh@gmail.com <gaddyh@gmail.com>
2023-07-05 03:31:35 +0300

    first net samples

--- code/prepareAudio.py
+++ code/prepareAudio.py
@@ -10,8 +10,8 @@ import random
 
 fr=48000
 top_db = 20
-n_mels=128
-fhat_units = fr/n_mels
+mels=256
+
 def length_sr(y1):
   if( y1.shape[0] < fr):
     zeros = math.floor((fr - y1.shape[0])/2)
@@ -33,51 +33,43 @@ def roll_rand(x):
     returns power spec binirized -20db shape:(35,94)
 '''
 def create_chroma(y):
-  threshold = -30
   a = create_power_spec(y)
   threshold = np.percentile(a, 98.9) 
-  print('percentile 75th of power spec D2 (threshold): ', threshold)
+  print('percentile 98.9 of power spec D2 (threshold): ', threshold)
   D2 = np.select([a <=threshold, a>threshold], [np.zeros_like(a), np.ones_like(a)])
+  D2 = D2[40:120,:]
   print('D2 shape: ', D2.shape)
-  return D2[0:70,:]
-  #return D2
+  return D2
 
 def create_power_spec(y):
-  threshold = np.percentile(y, 98) 
-  print(threshold*200)
-  y1 = librosa.effects.trim(y, top_db=40)[0]
-  y1 = length_sr(y1)
-  
-  fhat = np.fft.fft(y1, fr)
+  fhat = np.fft.fft(y, fr)
   fhat[0:700]=0
   fhat[4000:]=0
-
   a = fhat.real[:24000]
   print('fhat max:' , a.max())
   print('fhat max index:' , a.argmax())
   fhat[0:a.argmax() - 300] = 0  # when main freq is higher we can remove a higher range of lower frequncies
 
-
   yi = np.fft.ifft(fhat).astype(float)
-  #yi = librosa.effects.trim(yi, top_db=top_db)[0]
-  yi = length_sr(yi)
 
-  S2 = librosa.feature.melspectrogram(y=yi, sr=fr, n_mels=128)
+  S2 = librosa.feature.melspectrogram(y=yi, sr=fr, n_mels=mels)
   D2 = librosa.power_to_db(S2, ref=np.max)
   return D2
 
 def make_sample(file):
   y1o, sr = librosa.load(file, sr=fr)
-  print(sr)
   y1 = librosa.effects.trim(y1o, top_db=top_db)[0]
   y1 = length_sr(y1)
-  c = create_chroma(y1o)
+  
   fig, ax = plt.subplots(nrows=2, sharex=True)
   S2 = librosa.feature.melspectrogram(y=y1, sr=fr, n_mels=128)
   D2 = librosa.power_to_db(S2, ref=np.max)
   img = librosa.display.specshow(D2, x_axis='time', y_axis='mel', sr=sr, ax=ax[0])
   fig.colorbar(img, ax=[ax[0]])
   ax[0].label_outer()
+
+  c = create_chroma(y1)
+ 
   img = librosa.display.specshow(c, x_axis='time', y_axis='mel', sr=sr, ax=ax[1])
   fig.colorbar(img, ax=[ax[1]])
 
